456b29f4df9834831d843cd293f99f61d5fe54c4
Nicolas Hoffmann <dante3333@gmail.com>
2015-05-02 10:38:12 +0200

    Added comment, URL and license

--- jquery-accessible-tabs.js
+++ jquery-accessible-tabs.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+        /*
+         * jQuery Accessible tab panel system, using ARIA
+         * Website: http://a11y.nicolas-hoffmann.net/tabs/
+         * License MIT: https://github.com/nico3333fr/jquery-accessible-tabs-aria/blob/master/LICENSE
+         */
         // Store current URL hash.
         var hash = window.location.hash.replace( "#", "" );
  
